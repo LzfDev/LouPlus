1b7730bc345a89576245773a5562440d04066566
LzfDev <826435268@qq.com>
2018-12-29 23:49:48 +0800

    faker data

--- mul-to-mul.py
+++ mul-to-mul.py
@@ -1,11 +1,14 @@
 # coding: utf-8
-# new
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, Text
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table
+from faker import Factory
+from sqlalchemy.orm import sessionmaker
+import random
 
 
 engine = create_engine('mysql+pymysql://root@localhost:3306/blog?charset=utf8')
@@ -79,3 +82,33 @@ class Tag(Base):
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+
+    faker = Factory.create()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    faker_users = [User(
+        username=faker.name(),
+        password=faker.word(),
+        email=faker.email(),
+    ) for i in range(10)]
+    session.add_all(faker_users)
+
+    faker_categories = [Category(name=faker.word()) for i in range(5)]
+    session.add_all(faker_categories)
+
+    faker_tags= [Tag(name=faker.word()) for i in range(20)]
+    session.add_all(faker_tags)
+
+    for i in range(100):
+        article = Article(
+            title=faker.sentence(),
+            content=' '.join(faker.sentences(nb=random.randint(10, 20))),
+            author=random.choice(faker_users),
+            category=random.choice(faker_categories)
+        )
+        for tag in random.sample(faker_tags, random.randint(2, 5)):
+            article.tags.append(tag)
+        session.add(article)
+
+    session.commit()

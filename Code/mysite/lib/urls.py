347255f31f98f265eefa9857faa15369cd4886e7
LzfDev <826435268@qq.com>
2019-01-13 12:12:43 +0800

    mysite

--- Code/mysite/lib/urls.py
+++ Code/mysite/lib/urls.py
@@ -1,7 +1,11 @@
+# lib/urls.py
 from django.urls import path
 from . import views
 
+app_name = 'lib' #命名空间
 urlpatterns = [
         path('', views.index, name='index'),
         path('detail/', views.detail, name='detail'),
+        path('addBook/', views.addBook, name='addBook'),
+        path('delBook/<int:book_id>', views.delBook, name='delBook'),
 ]
--- Code/mysite/lib/views.py
+++ Code/mysite/lib/views.py
@@ -1,7 +1,10 @@
 # lib/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Book
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+from .models import Book
 
 # Create your views here.
 
@@ -13,3 +16,19 @@ def detail(request):
     context = {'book_list': book_list}
     return render(request, 'lib/detail.html', context)
 
+def addBook(request):
+    if request.method == 'POST':
+        temp_name = request.POST['name']
+        temp_author = request.POST['author']
+        temp_pub_house = request.POST['pub_house']
+
+    temp_book = Book(name=temp_name, author=temp_author, pub_house=temp_pub_house, pub_date=timezone.now())
+    temp_book.save()
+
+    # 重定向
+    return HttpResponseRedirect(reverse('lib:detail'))
+
+def delBook(request, book_id):
+    bookID = book_id
+    Book.objects.filter(id=bookID).delete()
+    return HttpResponseRedirect(reverse('lib:detail'))
